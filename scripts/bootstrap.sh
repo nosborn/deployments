93142801a82ff7d69084aa6456b32d6ce73a48b7
Nick Osborn <nosborn@users.noreply.github.com>
2026-05-07 07:52:07 +0100

    flux-cd: deploy automation components

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 cd "$(git rev-parse --show-toplevel)"
 
 mkdir -p clusters/tombstone/flux-system
-flux install --export >clusters/tombstone/flux-system/gotk-components.yaml
+flux install --components-extra=image-reflector-controller,image-automation-controller \
+  --export >clusters/tombstone/flux-system/gotk-components.yaml
