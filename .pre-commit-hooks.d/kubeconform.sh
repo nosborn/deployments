4cd56fd05c0a850b7dc94cc14ce721702f30763d
Nick Osborn <nosborn@users.noreply.github.com>
2026-05-09 08:33:45 +0100

    refactor: secrets

--- .pre-commit-hooks.d/kubeconform.sh
+++ .pre-commit-hooks.d/kubeconform.sh
@@ -4,6 +4,9 @@ mkdir -p .kubeconform
 
 kubeconform \
   -cache .kubeconform \
+  -ignore-filename-pattern /configs/ \
+  -ignore-filename-pattern /secret.yaml \
+  -ignore-filename-pattern /values.yaml \
   -ignore-missing-schemas \
   -output pretty \
   -schema-location default \
