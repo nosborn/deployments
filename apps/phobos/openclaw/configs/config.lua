77288979d436326ecdb298818b858eb50c3b4338
Nick Osborn <nosborn@users.noreply.github.com>
2026-05-18 16:09:02 +0100

    apps: configure OpenClaw

---
+++ apps/phobos/openclaw/configs/config.lua
@@ -0,0 +1,15 @@
+{
+	"data": "ENC[AES256_GCM,data:hac2txyxK+9eE8gxFDWWZ3aZUgidqMrMdKvEO5Xj5hhqGjD2fBUBqoetKn5BqvJx+gYTlk9MZQOJfb8X+q6VqqoaDyyzLfOU9lxz4y17cCxrA/H+pyMM/yv1Cg76fBwY9yKFEoerEH3UQY7cLm2ZK2CHFpMl/+2oUJkGYoZneic8ihIFWNe8zBMT/o1AClfwor9dvBBAx7rSWvI8z8zn+MyUE8lcdoTrsf6ufSk6q+bP2GE+3rbWVJip4F3ex+UnJWcoPz6QmT+JCQD/BcB9XDx/0Z5kBl5iR73/ipg6zUvUOyHcPEgQWfbkmi097XKN+2Iu7+3V27URxTiW4ufDleG9YP0siqCEMPwPtH9YBe8zvHEcPBzGsZKZxK32e03qTbPeyCL+Jjt40tYFPi0C2cs28B18D3UQhlOBnFfPSIINY79RTpbeKrq9+rsVv7KJZICv9OrC6NdjrUWpgIPe2byFmsBhB8txnr327quoYyYMdy2YXiK8C0DJ54Cb217YKKrynVnMZvZSuHkXHxThgihOumoWt2B4BJ56b0Dq6VRz4Az4WACqZYen1KPt+/6q3pHSQP9gTeWJjU/oq03/mqhyYwaupHPq6jzMEldLPoLzQMRGS2n0x+Obsx+8IWubEVK8BndOQStOK2Wsuhf1r2W64dnXau1Jfx0rhRyAQZzBWiN4bNssvXDR3v6bDs+158L1LMPErPRsZ5IU57prMgJ/h5c1wX5Le12sJIzcf/flYvnnpYjoVqRG/yCVAGreuM5+bE5BDNyiKTG8+g+mgdzFte3n9g6Hb7ShGSgPW7bQeHKCZ4Js1Cihjek6fIV5/dshgCyXlX/D3hMCcRwuuej15Uwa2Vka7s74aq/ZgsgiJ7fL5NTPdNLv7KovYx6IK4l5bJrkkJ8ji1gAbD9QyaRZiSyQ4zKzrIgaxRWEa1fLjfacpjBz3oJxAteEjslA4v4EBpEg1m9idM3i9dpYkBYDj9nsxa0YWNAbiaDIk9QXv+bWu5OnU8QcXNyn1m7AyojFyqedAot3pIJu3klhuNTOmdJNCy0GD6aZ+9Xrac0LU4bzjiw6sqqyHwFciUYDD8eCew==,iv:7883+NSk5cEOnirgW1qmEaU1pwZsrP/4u8BkStx+OfQ=,tag:K+SAM/wiNCC9hlPgbVv70w==,type:str]",
+	"sops": {
+		"age": [
+			{
+				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBHUE1aL1VUVUh2TUxUQTNS\nQzN2OVFSNEo0bjIyUWsrTjg2MkR0cmNvY0R3CjF6cXVLazBNb2dCMld6LzJCZFYv\nUnVwcjZKWUJEdWwvcjhRU0x5QlJ5dUUKLS0tIG1GLzZxNTJOZDdTWC9ZV2M5V0Zy\nQTEzSVkvV2Uxa2RTRytQRWxJaVhmZDgK9vK4qM38ffqwTmeADARq16/GvIANOfS8\ni+DxzI9GQZsc8ccSNVnooALYWPJGIrPglqyiSvrzaTJlePfc1hpYIQ==\n-----END AGE ENCRYPTED FILE-----\n",
+				"recipient": "age1cfvwwcvy3qj55kswlwymnvpwuwjymrp7xnjeycxma6qtdzv979qqd3kwue"
+			}
+		],
+		"lastmodified": "2026-05-19T16:24:16Z",
+		"mac": "ENC[AES256_GCM,data:qlqt4Ffj0tA9pKvUAVKFGziaLuqGW5d5G9oEKEw3lkmtxx8o8a3zKL7cs0oyl4yoYs485vVBcLDSacNJ2EgSxc67phY9KHR47DZ1fkak6kCbknIScWKbYB071iIsiV9z58JQRXsflJpHZhptW/h3chahRQXK+KPftWjd3DOfJhY=,iv:r0xAiDbWsIb11gx8NyhmiDkvzU8PZOZANg5wc6LJzho=,tag:AadUKbq71P06XgDRsOII+A==,type:str]",
+		"unencrypted_suffix": "_unencrypted",
+		"version": "3.13.1"
+	}
+}
