55339617ecbea6e9875dc6a17fc6a96621701955
Nick Osborn <nosborn@users.noreply.github.com>
2026-07-14 21:33:52 +0100

    apps: remove miasma

--- apps/base/miasma/scripts/fetch-content.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -eu
-
-curl -fLsS -o /tmp/html.zip https://github.com/nosborn/tombstone.osborn.io/archive/refs/heads/main.zip
-unzip -o /tmp/html.zip -d /usr/share/nginx/html
-rm /tmp/html.zip
